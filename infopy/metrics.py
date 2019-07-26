cc757ffea111780070746ad861f3de8b5ed79715
Chris Sweets <chris.sweet0320@gmail.com>
2019-07-26 15:53:33 -0400

    MIT License added

--- infopy/metrics.py
+++ infopy/metrics.py
@@ -1,4 +1,26 @@
 """
+Copyright (c) 2019 Christopher Sweet - crs4263@rit.edu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+"""
 Contains several metrics for comparing feature histograms. All methods are
 scalable to n-many combinations of features. Metrics include the following:
 
@@ -181,7 +203,7 @@ def joint_probability(p_hist:dict):
     p_dist = np.fromiter(p_cnt.values(), np.float)
     p_dist /= np.sum(p_dist)
     return p_dist
- 
+
 # def conditional_probability(histogram:dict, normalize:bool=False):
 #     """
 #     Computes the conditional entropy for all unique permutations given
